415a0f3848b6ca217615ba4192b0ed179ecfbeb4
nurafiniesuhaimi <89004947+nurafiniesuhaimi@users.noreply.github.com>
2021-08-19 11:32:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,12 @@ print(len(links))
 print("\n".join(links))
 
 #4-test case with apprioriate test funcions.
+import unittest
 
+class main(unittest.TestCase):
+
+    def test_Application(self):
+        print("Testing")
 
 
 
