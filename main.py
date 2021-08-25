dfcdfe41c55a674069580f46f46390bf784134ae
SimPeiQi <87391730+SimPeiQi@users.noreply.github.com>
2021-08-26 07:10:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,13 +33,33 @@ class NewSpider(scrapy.Spider):
 class NewSpider(scrapy.Spider):
         name = "new_spider"
         start_urls = ['https://brickset.com/sets/year-1998']
-        def parse(self, respond):
+        def parse(self, response):
             css_selector = 'img'
             for x in response.css(css_selector):
                 newsel = '@src'
                 yield {
                     'Image Link':x.xpath(newsel).extract_first(),
                 }
+                
+# havent indent . class NewSpider(scrapy.Spider):
+name = "new_spider"
+start_urls = ['http://192.168.1.1/index.html']
+def parse(self, response):
+xpath_selector = '//img'
+for x in response.xpath(xpath_selector):
+newsel = '@src'
+yield {
+'Image Link': x.xpath(newsel).extract_first(),
+}
+# To recurse next page
+ Page_selector = '.next a ::attr(href)'
+next_page = response.css(Page_selector).extract_first()
+if next_page:
+yield scrapy.Request(
+response.urljoin(next_page),
+callback=self.parse
+)
+
 #2.3-Store retrieve info in JSON
 
 import json
